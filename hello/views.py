1abb22c4d1aa670f37bb12e9fade1e5361341ce2
John Tordoff <johnetordoff@gmail.com>
2015-06-25 17:50:36 -0400

    made prog

--- hello/views.py
+++ hello/views.py
@@ -3,9 +3,36 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
+import git
+import os
+
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from John Tordoff!')
+    return render(request, 'blame.html')
+
+
+def blame(request):
+    repo = git.Repo("../osf.io")
+    retval = '<table width="100%">'
+    indent = ''
+    for commit, lines in repo.blame('HEAD', "website/static/js/"+request.POST.get('textfield', None)):
+        
+        
+        for line in lines:
+            line = line.replace(" ", "&nbsp")
+            retval += '<tr>'
+            retval += '<td >' + indent + str(line)+ '</td><td>' + str(commit.author) + '</td>'
+            retval += '</tr>'
+            try:
+                if line[-1] == "{":
+                    indent += "     "
+                else:
+                    indent = ""
+            except:
+                pass
+
+
+    return HttpResponse(retval)
 
 
 def db(request):
